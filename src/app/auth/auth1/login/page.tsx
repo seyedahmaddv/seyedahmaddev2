6663e2dd57d2c95ca6c80853b2ba08b9a3a06e98
Seyed Ahmad Gholami <seyedahmadqz@gmail.com>
2025-12-18 16:12:50 +0400

    Revert "login page"
    This reverts commit 5ce89c2d59b4ee2f827cf30ab4e2ea334aedfe49.

--- src/app/auth/auth1/login/page.tsx
+++ src/app/auth/auth1/login/page.tsx
@@ -88,7 +88,7 @@ export default function Login() {
                     variant="h6"
                     fontWeight="500"
                   >
-                    New to SeyedAhmad?
+                    New to Modernize?
                   </Typography>
                   <Link
                     href="/auth/auth1/register"
--- src/app/auth/auth2/login/page.tsx
+++ src/app/auth/auth2/login/page.tsx
@@ -60,7 +60,7 @@ export default function Login2() {
                       variant="h6"
                       fontWeight="500"
                     >
-                      New to SeyedAhmad?
+                      New to Modernize?
                     </Typography>
                     <Link
                       href="/auth/auth2/register"
